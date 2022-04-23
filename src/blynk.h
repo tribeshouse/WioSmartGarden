911d10a2a2d73e30beda5027386d752ef5a80b88
Benjamin <brichter@tribeshouse.com>
2022-04-23 19:22:53 +0200

    Add Blynk

---
+++ src/blynk.h
@@ -0,0 +1,77 @@
+#ifndef BLYNK_H
+#define BLYNK_H
+
+#include "BlynkParamPlus.h"
+
+#define BLYNK_TEMPLATE_ID "TMPLtRVWKVjd"
+#define BLYNK_DEVICE_NAME "Smart Garden"
+
+#define BLYNK_FIRMWARE_VERSION "0.1.0"
+
+#define SERIAL_DEBUG // Comment this out to disable Serial outputs
+// #define TERMINAL_DEBUG
+
+// #define BLYNK_PRINT
+#define BLYNK_PRINT Serial
+#define BLYNK_DEBUG
+
+#define APP_DEBUG
+
+// #define BLYNK_NO_BUILTIN // Disable built-in analog & digital pin operations
+// #define BLYNK_NO_FLOAT // Disable float operations
+
+#if defined(SERIAL_DEBUG)
+#define TERMINAL_BAUD 115200
+#define BLYNK_PRINT Serial // Print debug outputs to Serial port
+// #define APP_DEBUG // Comment this out to disable debug prints
+// #define BLYNK_DEBUG        // Optional, this enables more detailed prints
+#define DEBUG(ARG) Serial.print(ARG)
+#define DEBUGLN(ARG) Serial.println(ARG)
+#elif defined(TERMINAL_DEBUG)
+WidgetTerminal terminal(TERMINAL);
+#define DEBUG(ARG) terminal.print(ARG)
+#define DEBUGLN(ARG) terminal.println(ARG)
+#elif defined(TERMINAL_SERIAL_DEBUG)
+WidgetTerminal terminal(TERMINAL);
+#define DEBUG(ARG)     \
+    Serial.print(ARG); \
+    terminal.print(ARG)
+#define DEBUGLN(ARG)     \
+    Serial.println(ARG); \
+    terminal.println(ARG)
+#else
+#define DEBUG
+#define DEBUGLN
+#define BLYNK_PRINT
+#endif
+
+#if defined(APP_DEBUG)
+#define DEBUG_PRINT(...) BLYNK_LOG1(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
+
+#include "BlynkEdgent.h"
+
+void vWrite(int pin, BlynkParam &param);
+void blynk_writeConfig();
+
+enum pins
+{
+    TERMINAL = V0,
+    PIN_MOIST = V1,
+    PIN_HUM = V2,
+    PIN_TEMP = V3,
+    PIN_LIGHT = V4,
+    // add more Pins here
+    PIN_NUM // last item, number of pins
+};
+
+// digital pins
+// int pumpPin = 3;
+
+// Params for each pin are stored here and can be accessed via e.g.:
+// value = pinParams[pin].asInt();
+BlynkParamPlus pinParams[PIN_NUM];
+
+#endif #BLYNK_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // Based on https://github.com/lakshanthad/Wio_Terminal_Classroom_Arduino/blob/main/Classroom%2012/Smart_Garden/Smart_Garden.ino
 
-#include <Arduino.h>
+#include "blynk.h"
 #include "SHT31.h"
 
 //Libraries
@@ -8,86 +8,115 @@
 //Initializations
 SHT31 sht31 = SHT31();
 TFT_eSPI tft; //Initializing TFT LCD
-TFT_eSprite spr = TFT_eSprite(&tft); //Initializing buffer
+#define LCD_BACKLIGHT (72Ul) // Control Pin of LCD
+#define LCD_TIMEOUT 10000L
+#define UPDATE_FEQUENCY 30000L
 
-//Soil Moisture Sensor definitions
+#include <Button.h>
+
+Button button_press(WIO_5S_PRESS); 
+
+// Soil Moisture Sensor definitions
 int sensorPin = A0; //Define variable to store soil moisture sensor pin
 int sensorValue = 0; //Define variable to store soil moisture sensor value
 int waterThreshold = 20;
+#define SENSOR_MAX 1000
+
+void update()
+{
+  // Setting temperature
+  int t = sht31.getTemperature();         // Assign variable to store temperature
+  tft.setTextColor(TFT_WHITE, TFT_BLACK); // Setting text color
+  tft.setTextSize(2);
+  tft.drawString("Temperature", 10, 65);
+  tft.setTextSize(3);
+  tft.drawNumber(t, 50, 95); // Display temperature values
+  tft.drawString("°C", 90, 95);
+
+  // Setting humidity
+  int h = sht31.getHumidity(); // Assign variable to store humidity
+  tft.setTextSize(2);
+  tft.drawString("Humidity", 25, 160);
+  tft.setTextSize(3);
+  tft.drawNumber(h, 30, 190); // Display humidity values
+  tft.drawString("%RH", 70, 190);
+
+  // Setting soil moisture
+  sensorValue = analogRead(sensorPin); // Store sensor values
+  // sensorValue = map(sensorValue,650,250,0,100); //Map sensor values
+  tft.setTextSize(2);
+  tft.drawString("Soil Moisture", 160, 65);
+  tft.setTextSize(3);
+  tft.drawNumber(sensorValue, 200, 95); // Display sensor values as percentage
+  // tft.drawString("%",260,95);
+
+  // Setting light
+  int light = analogRead(WIO_LIGHT); // Assign variable to store light sensor values
+  tft.setTextSize(2);
+  tft.drawString("Light", 200, 160);
+  tft.setTextSize(3);
+  light = map(light, 0, 1023, 0, 100); // Map sensor values
+  tft.drawNumber(light, 205, 190);     // Display sensor values as percentage
+  tft.drawString("%", 260, 190);
+
+  // //Condition for low soil moisture
+  // if(sensorValue < waterThreshold){
+  //   tft.fillSprite(TFT_RED);
+  //   tft.drawString("Time to water!",35,100);
+  //   if (light > 1) {    //Don't beep at night
+  //     analogWrite(WIO_BUZZER, 150); //beep the buzzer
+  //     delay(1000);
+  //     analogWrite(WIO_BUZZER, 0); //Silence the buzzer
+  //     delay(1000);
+  //   }
+  // }
+
+
+  Blynk.virtualWrite(V1, sensorValue);
+  Blynk.virtualWrite(V2, h);
+  Blynk.virtualWrite(V3, t);
+  Blynk.virtualWrite(V4, light);
+}
+
+void idle_lcd()
+{
+  digitalWrite(LCD_BACKLIGHT, LOW);
+}
 
 void setup() {
-  Serial.begin(9600); //Start serial communication
+#if defined(SERIAL_DEBUG)
+  Serial.begin(TERMINAL_BAUD);
+#endif
 
   pinMode(sensorPin, INPUT);
   pinMode(WIO_LIGHT, INPUT); //Set light sensor pin as INPUT
   pinMode(WIO_BUZZER, OUTPUT); //Set buzzer pin as OUTPUT
 
+  button_press.begin();
+
   sht31.begin(); //Start SHT sensor
   tft.begin(); //Start TFT LCD
   tft.setRotation(3); //Set TFT LCD rotation
-  spr.createSprite(TFT_HEIGHT,TFT_WIDTH); //Create buffer
+  tft.fillScreen(TFT_BLACK); // fills entire the screen with colour
+  //Setting the title header
+  tft.fillRect(0,0,320,50,TFT_DARKGREEN); //Rectangle fill with dark green
+  tft.setTextColor(TFT_WHITE, TFT_DARKGREEN);       // Setting text color
+  tft.setTextSize(3);                     // Setting text size
+  tft.drawString("Smart Garden", 50, 15); // Drawing a text string
+  tft.drawFastVLine(150, 50, 190, TFT_DARKGREEN); // Drawing verticle line
+  tft.drawFastHLine(0, 140, 320, TFT_DARKGREEN);  // Drawing horizontal line
+
+  edgentTimer.setInterval(UPDATE_FEQUENCY, update);
+  edgentTimer.setTimeout(LCD_TIMEOUT, idle_lcd);
+  BlynkEdgent.begin();
 }
 
-void loop() {
-  //Setting the title header
-  spr.fillSprite(TFT_WHITE); //Fill background with white color
-  spr.fillRect(0,0,320,50,TFT_DARKGREEN); //Rectangle fill with dark green
-  spr.setTextColor(TFT_WHITE); //Setting text color
-  spr.setTextSize(3); //Setting text size
-  spr.drawString("Smart Garden",50,15); //Drawing a text string
-
-  spr.drawFastVLine(150,50,190,TFT_DARKGREEN); //Drawing verticle line
-  spr.drawFastHLine(0,140,320,TFT_DARKGREEN); //Drawing horizontal line
-
-  //Setting temperature
-  int t = sht31.getTemperature(); //Assign variable to store temperature
-  spr.setTextColor(TFT_BLACK);
-  spr.setTextSize(2);
-  spr.drawString("Temperature",10,65);
-  spr.setTextSize(3);
-  spr.drawNumber(t,50,95); //Display temperature values
-  spr.drawString("C",90,95);
-
-  //Setting humidity
-  int h = sht31.getHumidity(); //Assign variable to store humidity
-  spr.setTextSize(2);
-  spr.drawString("Humidity",25,160);
-  spr.setTextSize(3);
-  spr.drawNumber(h,30,190); //Display humidity values
-  spr.drawString("%RH",70,190);
-
-  //Setting soil moisture
-  sensorValue = analogRead(sensorPin); //Store sensor values
-  sensorValue = map(sensorValue,650,250,0,100); //Map sensor values
-  spr.setTextSize(2);
-  spr.drawString("Soil Moisture",160,65);
-  spr.setTextSize(3);
-  spr.drawNumber(sensorValue,200,95); //Display sensor values as percentage
-  spr.drawString("%",240,95);
-
-  //Setting light
-  int light = analogRead(WIO_LIGHT); //Assign variable to store light sensor values
-  spr.setTextSize(2);
-  spr.drawString("Light",200,160);
-  spr.setTextSize(3);
-  light = map(light,0,1023,0,100); //Map sensor values
-  spr.drawNumber(light,205,190); //Display sensor values as percentage
-  spr.drawString("%",245,190);
-
-  //Condition for low soil moisture
-  if(sensorValue < waterThreshold){
-    spr.fillSprite(TFT_RED);
-    spr.drawString("Time to water!",35,100);
-    if (light > 1) {    //Don't beep at night
-      analogWrite(WIO_BUZZER, 150); //beep the buzzer
-      delay(1000);
-      analogWrite(WIO_BUZZER, 0); //Silence the buzzer
-      delay(1000);
-    }
+void loop()
+{
+  if (button_press.pressed())
+  {
+    digitalWrite(LCD_BACKLIGHT, HIGH);
+    edgentTimer.setTimeout(LCD_TIMEOUT, idle_lcd);
   }
-
-  spr.pushSprite(0,0); //Push to LCD
-  delay(1000);
-
-
+  BlynkEdgent.run();
 }
